05f01076e9621e614d297360a36e33493d76772c
mhmatthall <mhmatthall@yahoo.com>
2019-02-12 00:20:12 +0000

    Made startup sequence better

--- src/Photoshop2019.java
+++ src/Photoshop2019.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -6,7 +8,7 @@ import javafx.stage.Stage;
 
 public class Photoshop2019 extends Application {
 	Parent root;
-	Stage stage;
+	static Stage stage;
 	
 	public static void main(String[] args) {
 		launch();
@@ -15,18 +17,27 @@ public class Photoshop2019 extends Application {
 	@Override
 	public void start(Stage mainStage) {
 		try {
-			root = FXMLLoader.load(getClass().getResource("fxml/main.fxml"));
+			// Retrieve the FXML file for the main window
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/main.fxml"));
+			// Try and load the file
+			root = loader.load();
 			
+			// Assign and customise the stage
 			stage = mainStage;
 			stage.setTitle("Photoshop Weyy");
 			
-			Scene scene = new Scene(root);
+			// Pass the stage into the controller so we can easily use it
+			MainViewController controller = loader.getController();
+			controller.setStage(stage);
 			
+			// Creating the main scene and displaying it
+			Scene scene = new Scene(root);
 			stage.setScene(scene);
 			stage.show();
 			
-		} catch (Exception ex) {
-			System.out.println("-ERROR----------------------------");
+		} catch (IOException ex) {
+			// If no such FXML file could be found
+			System.out.println("-ERROR: MAIN FXML FILE NOT FOUND------------------");
 			ex.printStackTrace();
 		}		
 	}
