f5043246e17c18c3c2613d21278b3ae4e65dd20a
mhmatthall <mhmatthall@yahoo.com>
2019-02-12 00:19:09 +0000

    Added UI functions; opening a file

--- src/MainViewController.java
+++ src/MainViewController.java
@@ -1,29 +1,31 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
+import java.util.Arrays;
 
-import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.chart.AreaChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SplitPane;
-import javafx.scene.effect.BlendMode;
-import javafx.scene.effect.Bloom;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class MainViewController {
+	private final String[] ACCEPTED_FILE_EXTENSIONS = { "*.png", "*.jpg", "*.jpeg", "*.bmp", ".gif" };
+
+	private Stage stage;
 
 	@FXML
 	private MenuItem menuFileNew;
@@ -51,21 +53,24 @@ public class MainViewController {
 
 	@FXML
 	private Menu menuAbout;
-	
+
 	@FXML
 	private SplitPane pneMainSplit;
 
-    @FXML
-    private AnchorPane pneToolPane;
-	
-    @FXML
-    private Pane pneToolSelector;
-    
+	@FXML
+	private AnchorPane pneToolPane;
+
+	@FXML
+	private Pane pneToolSelector;
+
 	@FXML
 	private ImageView btnGamma;
 
 	@FXML
 	private ImageView btnContrast;
+
+	@FXML
+	private ImageView imgImageViewer;
 
 	@FXML
 	private Label lblCurrentToolName;
@@ -88,72 +93,100 @@ public class MainViewController {
 	@FXML
 	private Color x4;
 
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
+
+	// FXML Constructor
 	@FXML
-	void btnGammaClicked(MouseEvent event) {
-		// Visual effects when tool selected
-		lblCurrentToolName.textProperty().set("Gamma Correction");
-		resetButtonEffects();
-		btnGamma.setOpacity(1);		// Highlight the selected tool icon
-		btnGamma.setScaleX(1.1);
-		btnGamma.setScaleY(1.1);
-		
-		// Loading the selected pane's content
-		Pane newPane;
-		try {
-			newPane = FXMLLoader.load(getClass().getResource("fxml/menuGamma.fxml"));	// Load the new pane's layout
-			pneToolPane.getChildren().add(newPane);	// Add the nodes to the blank pane
-		} catch (IOException ex) {
-			System.out.println("FXML FILE FOR THIS MENU NOT FOUND");
-			ex.printStackTrace();
+	public void initialize() {
+		// Select the gamma menu as the default when the program loads
+		btnGammaClicked(null);
+	}
+
+	// Toolbar menu methods
+	@FXML
+	void openFile(ActionEvent event) throws FileNotFoundException {
+		FileChooser picker = new FileChooser();
+
+		picker.setTitle("Open Image File");
+
+		// Filter out incompatible files from the dialog search
+		FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter(
+				"Image files (*.jpg, *.jpeg, *.png, *.bmp, *.gif)", // Description shown to user
+				Arrays.asList(ACCEPTED_FILE_EXTENSIONS)); // File extension mask
+		picker.getExtensionFilters().add(filter);
+
+		// Open the dialog
+		File chosenImage = picker.showOpenDialog(stage);
+
+		if (!chosenImage.equals(null)) {
+			// If the user selects an image
+			Image img = new Image(new FileInputStream(chosenImage));
+			imgImageViewer.setImage(img); // Load the image onto the main image editor
+
+			stage.setTitle("Photoshop - '" + chosenImage.getName() + "'");
 		}
 	}
-	
+
+	@FXML
+	void closeProgram(ActionEvent event) {
+		stage.close();
+	}
+
+	// Gamma Correction menu methods
+	@FXML
+	void btnGammaClicked(MouseEvent event) {
+		changeTool("fxml/menuGamma.fxml");
+	}
+
 	@FXML
 	void btnGammaStartHover(MouseEvent event) {
-		hoverEffect((ImageView) event.getTarget());
+		ImageView img = (ImageView) event.getTarget();
+		/*
+		 * TODO create new images for the buttons: +inactive +active +hovered
+		 */
+
 	}
-	
+
 	@FXML
 	void btnGammaEndHover(MouseEvent event) {
-		hoverEffect((ImageView) event.getTarget());
+		ImageView img = (ImageView) event.getTarget();
 	}
-	
+
+	// Contrast Stretching menu methods
 	@FXML
 	void btnContrastClicked(MouseEvent event) {
-		
+		changeTool("fxml/menuContrast.fxml");
 	}
 
 	@FXML
 	void btnContrastStartHover(MouseEvent event) {
-		hoverEffect((ImageView) event.getTarget());
+		ImageView img = (ImageView) event.getTarget();
 	}
-	
+
 	@FXML
 	void btnContrastEndHover(MouseEvent event) {
-		hoverEffect((ImageView) event.getTarget());
-	}
-	
-	private void hoverEffect(ImageView img) {
-		if (img.getScaleX() == 1) {
-			Bloom b = new Bloom();
-			b.setThreshold(0.2);
-
-			if (img.getOpacity() == 0.5) {
-				img.setOpacity(1);
-				img.setEffect(b);
-			} else {
-				img.setOpacity(0.5);
-				img.setEffect(null);
-			}
-		}
+		ImageView img = (ImageView) event.getTarget();
 	}
 
-	private void resetButtonEffects() {
-		ObservableList<Node> list = pneToolSelector.getChildren();
-		List<ImageView> buttons = getNodesOfType(pneToolSelector, ImageView.class);
-		for (Node n : list) {
-			n.setScaleX(1);
-			n.setScaleY(1);
+	// Other menu methods
+	/**
+	 * Loads a new tool menu panel form into the tool menu region
+	 * 
+	 * @param fileLocation The location of the tool's corresponding FXML file
+	 */
+	private void changeTool(String fileLocation) {
+		Pane newPane;	// Create a blank pane
+		try {
+			// Load the new tool onto the blank pane
+			newPane = FXMLLoader.load(getClass().getResource(fileLocation));
+			pneToolPane.getChildren().add(newPane); // Add the nodes to the new pane
+
+		} catch (IOException ex) {
+			// If no such FXML file could be found
+			System.out.println("-ERROR: TOOL FXML FILE NOT FOUND------------------");
+			ex.printStackTrace();
 		}
 	}
 }
