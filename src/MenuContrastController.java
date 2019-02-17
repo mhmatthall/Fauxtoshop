5da458c0290ddda3d9e5bed93d06e2b2fed4fd24
mhmatthall <mhmatthall@yahoo.com>
2019-02-17 16:11:33 +0000

    Added blank contrast tool controller

---
+++ src/MenuContrastController.java
@@ -0,0 +1,124 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.shape.Circle;
+
+public class MenuContrastController extends ToolController {
+    @FXML
+    private Circle node1;
+
+    @FXML
+    private Circle node2;
+
+    @FXML
+    private Canvas cvsContrastStretcher;
+
+    @FXML
+    private TextField txtNode1In;
+
+    @FXML
+    private Slider sldNode1In;
+
+    @FXML
+    private TextField txtNode1Out;
+
+    @FXML
+    private Slider sldNode1Out;
+
+    @FXML
+    private TextField txtNode2In;
+
+    @FXML
+    private Slider sldNode2In;
+
+    @FXML
+    private TextField txtNode2Out;
+
+    @FXML
+    private Slider sldNode2Out;
+
+    @FXML
+    void node1Clicked(MouseEvent event) {
+    	
+    }
+
+    @FXML
+    void node1Dragged(MouseEvent event) {
+
+    }
+
+    @FXML
+    void node1InSliderUpdated(MouseEvent event) {
+
+    }
+
+    @FXML
+    void node1InTextChanged(ActionEvent event) {
+
+    }
+
+    @FXML
+    void node1InTextScrolled(ScrollEvent event) {
+
+    }
+
+    @FXML
+    void node1OutSliderUpdated(MouseEvent event) {
+
+    }
+
+    @FXML
+    void node1OutTextChanged(ActionEvent event) {
+
+    }
+
+    @FXML
+    void node1OutTextScrolled(ScrollEvent event) {
+
+    }
+
+    @FXML
+    void node2Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void node2Dragged(MouseEvent event) {
+
+    }
+
+    @FXML
+    void node2InSliderUpdated(MouseEvent event) {
+
+    }
+
+    @FXML
+    void node2InTextChanged(ActionEvent event) {
+
+    }
+
+    @FXML
+    void node2InTextScrolled(ScrollEvent event) {
+
+    }
+
+    @FXML
+    void node2OutSliderUpdated(MouseEvent event) {
+
+    }
+
+    @FXML
+    void node2OutTextChanged(ActionEvent event) {
+
+    }
+
+    @FXML
+    void node2OutTextScrolled(ScrollEvent event) {
+
+    }
+
+}
