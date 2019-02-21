d183ddcd042f1e89a6768bd1db5c0328dc62dedc
Matt <42594962+mhmatthall@users.noreply.github.com>
2019-02-21 11:49:08 +0000

    Started rich contrast stretching control

--- src/MenuContrastController.java
+++ src/MenuContrastController.java
@@ -1,13 +1,20 @@
+import java.awt.Point;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 public class MenuContrastController extends ToolController {
+	private Point nodeOrigin = new Point();
+	
     @FXML
     private Circle node1;
 
@@ -15,7 +22,7 @@ public class MenuContrastController extends ToolController {
     private Circle node2;
 
     @FXML
-    private Canvas cvsContrastStretcher;
+    private Rectangle stretchArea;
 
     @FXML
     private TextField txtNode1In;
@@ -47,10 +54,20 @@ public class MenuContrastController extends ToolController {
     }
 
     @FXML
-    void node1Dragged(MouseEvent event) {
-
+    void node1StartDrag(MouseEvent event) {
+    	startNodeDrag(node1, event);
     }
-
+    
+    @FXML
+    void node1Dragged(MouseEvent event) {
+    	dragNode(node1, event);
+    }
+    
+    @FXML
+    void node1EndDrag(MouseEvent event) {
+    	endNodeDrag(node1, event);
+    }
+    
     @FXML
     void node1InSliderUpdated(MouseEvent event) {
 
@@ -83,12 +100,22 @@ public class MenuContrastController extends ToolController {
 
     @FXML
     void node2Clicked(MouseEvent event) {
-
+    	// right-click to reset position to default
     }
 
     @FXML
+    void node2StartDrag(MouseEvent event) {
+    	startNodeDrag(node2, event);
+    }
+    
+    @FXML
     void node2Dragged(MouseEvent event) {
-
+    	dragNode(node2, event);
+    }
+    
+    @FXML
+    void node2EndDrag(MouseEvent event) {
+    	endNodeDrag(node2, event);
     }
 
     @FXML
@@ -121,4 +148,20 @@ public class MenuContrastController extends ToolController {
 
     }
 
+    private void startNodeDrag(Circle node, MouseEvent event) {
+    	double x = node.getLayoutX(); //+ node.getCenterX();
+    	double y = node.getLayoutY(); //+ node.getCenterY();
+    	nodeOrigin.setLocation(x, y);
+    	node.setCursor(Cursor.CLOSED_HAND);
+    }
+    
+    private void dragNode(Circle node, MouseEvent event) {
+    	node.setCenterX(event.getX() - nodeOrigin.getX());
+    	node.setCenterY(event.getY() - nodeOrigin.getY());
+    }
+    
+    private void endNodeDrag(Circle node, MouseEvent event) {
+    	node.setCursor(Cursor.HAND);
+    }
+    
 }
