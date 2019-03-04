3eb4bd37534fbd1cfb3aed15fac5643d28eaa7b2
mhmatthall <mhmatthall@yahoo.com>
2019-03-04 04:34:09 +0000

    Renamed launch file

--- src/Photoshop.java
+++ src/Photoshop.java
@@ -7,14 +7,21 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class Photoshop2019 extends Application {
+/**
+ * Main launcher for the CS-255 graphics coursework.
+ * <p>
+ * I declare that the following is my own work.
+ * 
+ * @author Matt Hall (961500)
+ */
+public class Photoshop extends Application {
 	Parent root;
 	static Stage stage;
-	
+
 	public static void main(String[] args) {
 		launch();
 	}
-	
+
 	@Override
 	public void start(Stage mainStage) {
 		try {
@@ -22,31 +29,31 @@ public class Photoshop2019 extends Application {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/main.fxml"));
 			// Try and load the file
 			root = loader.load();
-			
+
 			// Assign and customise the stage
 			stage = mainStage;
-			stage.setTitle("Photoshop");
-			
+			stage.setTitle("Fauxtoshop");
+
 			// Load the program icons of various sizes
 			stage.getIcons().add(new Image("ico/logo256.png"));
 			stage.getIcons().add(new Image("ico/logo48.png"));
 			stage.getIcons().add(new Image("ico/logo32.png"));
 			stage.getIcons().add(new Image("ico/logo24.png"));
 			stage.getIcons().add(new Image("ico/logo16.png"));
-			
+
 			// Pass the stage into the controller so we can easily use it
 			MainViewController controller = loader.getController();
 			controller.setStage(stage);
-			
+
 			// Creating the main scene and displaying it
 			Scene scene = new Scene(root);
 			stage.setScene(scene);
 			stage.show();
-			
+
 		} catch (IOException ex) {
 			// If no such FXML file could be found
 			System.out.println("-ERROR: MAIN FXML FILE NOT FOUND------------------");
 			ex.printStackTrace();
-		}		
+		}
 	}
 }
